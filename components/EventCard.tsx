2d08a5ee61e768edaa860eca94a131d2f6aa8e11
Ryan DuVal <futzlarson@gmail.com>
2026-01-14 22:53:06 -0500

    Update 'Flag incorrect info' toast

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -260,7 +260,7 @@ export default function EventCard({
 
   const handleReport = (reportType: 'incorrect_info' | 'duplicate' | 'spam') => {
     setMoreMenuOpen(false);
-    showToast('Thanks for the feedback!');
+    showToast('Thanks for the feedback! This event will be automatically reviewed for correctness.');
     fetch('/api/events/report', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
